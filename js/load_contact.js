355420aa83e50d2696c51f347901862e161870dc
dennis.zhao <dennis.zhao@wewowwe.com>
2016-08-19 11:36:14 +0800

    support mobile header

--- js/load_contact.js
+++ js/load_contact.js
@@ -4,4 +4,10 @@
 
 $(document).ready(function() {
     $('#header').css('top', '0px');
+    $('#header-mobile').css('top', '0px');
+
+    $('.navigation').click(function(){
+        $(this).attr('aria-expanded','false');
+        $(this).removeClass('in');
+    });
 });
